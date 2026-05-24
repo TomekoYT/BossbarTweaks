ec589b5c0cece4b94bf58f975f1189c8052affa3
TomekoYT <tomekoyt@interia.pl>
2026-05-24 14:58:28 +0200

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+
+        maven("https://maven.fabricmc.net")
+        maven("https://jitpack.io/")
+        maven("https://maven.architectury.dev")
+        maven("https://maven.kikugie.dev/snapshots")
+        maven("https://maven.kikugie.dev/releases")
+
+        maven("https://maven.deftu.dev/releases")
+        maven("https://maven.deftu.dev/snapshots")
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version providers.gradleProperty("stonecutter_version")
+}
+
+stonecutter {
+    create(rootProject) {
+        version("1.21.9")
+        version("26.1").buildscript("build.unobfuscated.gradle.kts")
+        vcsVersion = "1.21.9"
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs")
+    }
+}
+
+rootProject.name = "BossbarTweaks"
---
+++ src/main/java/tomeko/bossbartweaks/BossbarTweaks.java
@@ -0,0 +1,10 @@
+package tomeko.bossbartweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BossbarTweaks implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
